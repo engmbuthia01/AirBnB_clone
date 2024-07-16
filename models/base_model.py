5d92e91f4248b9aa44d0d24c2f00f8905ff5963d
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-07-15 19:35:58 -0700

    class BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,56 +1,37 @@
 #!/usr/bin/python3
+
 """
-This module defines the base model
-defines all common attributes/methods for other classes
+This module defines the a class BaseModel
+that defines all common attributes/methods
+for other classes
 """
 
 import uuid
-from datetime import datetime
-import models
+import datetime
 
 
 class BaseModel:
-    """parent class"""
-
+    """
+    defines all common attributes/methods for other classes
+    """
     def __init__(self, *args, **kwargs):
-       """initializes the newly created objects"""
-
-       self.id = str(uud.uud4())
-       self.created_at = datetime.now()
-       self.updated_at = datetime.now()
-       tformat = "%Y-%m-%dT%H:%M:%S.%f"
-
-       if len(kwargs) != 0:
-           for k, v in kwargs.items():
-               if k == "created_at" or k == "updated_at":
-                   self.__dict__[k] = datetime.strptime(v, tformat)
-               else:
-                   self.__dict__[k] = v
-        else:
-            models.storage.new(self)
-
-    def save(self):
-        """
-        updates the public instance attribute updated_at
-        with the current datetime
-        """
-
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        models.storage.save()
-
-    def to_dict(self):
-        """
-        returns a dictionary containing all keys/values
-        of __dict__ of the instance
-        """
-
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
 
     def __str__(self):
-        """String representation of a BaseModel instance"""
-        return ("[{}] ({}) {}".format(self.__class__.__name__,
-                                      self.id, self.__dict__))
+        """string representation"""
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+        """updates, updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/valu of __dict__"""
+        new_dict = dict(self.__dict__)
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+
+        return (new_dict)
--- models/city.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""This moduledefines a class City"""
-
-from models.base_model import BaseModel
-
-
-class City(BaseModel):
-    state_id = ""
-    name = ""
