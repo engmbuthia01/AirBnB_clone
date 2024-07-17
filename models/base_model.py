8f3c6a128428193e7dc504ddd0eb1069b30a10ee
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-07-17 02:07:20 -0700

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -17,30 +17,28 @@ class BaseModel:
     """
 
     def __init__(self, *args, **kwargs):
-        """Initialization"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
 
-            tform = "%Y-%m-%dT%H:%M:%S.%f"
-
-            if len(kwargs) != 0:
-                for k, v in kwargs.items():
-                    if k == "created_at" or k == "updated_at":
-                        self.__dict__[k] = datetime.strptime(v, tform)
-                    else:
-                        self.__dict__[k] = v
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
 
     def __str__(self):
         """string representation"""
-        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
-
+        clsname = self.__class__.__name__
+        return ("[{}] ({}) {}".format(clsname, self.id, self.__dict__))
 
     def save(self):
         """updates, updated_at with the current datetime"""
         self.updated_at = datetime.now()
-    
+
     def to_dict(self):
         """dictionary respresentation"""
         new_dict = dict(self.__dict__)
