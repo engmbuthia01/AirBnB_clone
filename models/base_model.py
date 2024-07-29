ce7c5768ef752b7a9fb3aa7ab2466226050144f4
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-07-28 22:00:53 -0700

    class BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,6 @@ that defines all common attributes/methods
 for other classes
 """
 
-import models
 import uuid
 import datetime
 
@@ -16,19 +15,12 @@ class BaseModel:
     defines all common attributes/methods for other classes
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
+        """Initializing"""
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
-
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
-                else:
-                    self.__dict__[k] = v
 
     def __str__(self):
         """string representation"""
@@ -41,7 +33,7 @@ class BaseModel:
 
     def to_dict(self):
         """dictionary respresentation"""
-        new_dict = dict(self.__dict__)
+        new_dict = self.__dict__.copy()
         new_dict['__class__'] = self.__clas__.__name__
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
