ad23df86ba44f361c06ac8b6811104e6a62f3f6a
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-08-16 02:49:27 -0700

    defines all common attributes/methods for other classes

--- models/base_model.py
+++ models/base_model.py
@@ -21,4 +21,19 @@ class Basemodel:
         """
         self.updated_at = datetime.utcnow()
 
-    def to_dict(self)
+    def to_dict(self):
+        """serialization"""
+        inst_dict = self.__dict__.copy()
+        inst_dict["__class__"] = self.__class__.__name__
+        inst_dict["created_at"] = self.created_at.isoformat()
+        inst_dict["updated_at"] = self.updated_at.isoformat()
+
+        return inst_dict
+
+    def __str__(self):
+        """
+        string representation of
+        the class
+        """
+        class_name = self.__class__.name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
