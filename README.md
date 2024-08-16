ffae58ea5262cd53ea446e54b572ce5009d1d75b
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-08-16 00:29:52 -0700

    progress

--- README.md
+++
@@ -1,5 +0,0 @@
-AirBnB clone - The console
-
-Project description.
-===================
-This is the first part of the AIRBNB clone web application.
--- models/base_model.py
+++ models/base_model.py
@@ -1,54 +1,24 @@
 #!/usr/bin/python3
-
 """
-This module defines the a class BaseModel
-that defines all common attributes/methods
-for other classes
+This module defines the class
+Basemodel
 """
 
 import uuid
-import datetime
+from datetime import datetime
 
 
-class BaseModel:
-    """
-    defines all common attributes/methods for other classes
-    """
-
+class Basemodel:
     def __init__(self):
-        """Initializing"""
-
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-    def __str__(self):
-        """string representation"""
-        clsname = self.__class__.__name__
-        return ("[{}] ({}) {}".format(clsname, self.id, self.__dict__))
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
 
     def save(self):
-        """updates, updated_at with the current datetime"""
-        self.updated_at = datetime.now()
+        """
+        updates the updated_at attribute with the
+        current datetime
+        """
+        self.updated_at = datetime.utcnow()
 
-    def to_dict(self):
-        """dictionary respresentation"""
-        new_dict = self.__dict__.copy()
-        new_dict['__class__'] = self.__clas__.__name__
-        new_dict['created_at'] = self.created_at.isoformat()
-        new_dict['updated_at'] = self.updated_at.isoformat()
-
-        return (new_dict)
-
-if __name__ == "__main__"
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+    def to_dict(self)
